5e226d7d32654591a40b247228a550bd42340cfc
awdaxyz <vincentiushendru1508@gmail.com>
2026-01-13 22:40:33 +0700

    Update Index.js

--- routes/Index.js
+++ routes/Index.js
@@ -1,8 +1,10 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
 
 module.exports = (app) => {
+
     app.use(express.urlencoded({ extended: false }));
+    app.use(express.json());
 
     app.use((req, res, next) => {
         res.header('Access-Control-Allow-Origin', '*');
