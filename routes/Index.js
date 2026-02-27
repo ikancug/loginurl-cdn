65e292ef3958c3ad2ee9bdc3e8be98815a7c7b07
ikancug <ikandiscord@gmail.com>
2026-02-27 22:12:31 +0700

    Update Index.js

--- routes/Index.js
+++ routes/Index.js
@@ -4,7 +4,6 @@ const path = require('path');
 module.exports = (app) => {
 
     app.use(express.urlencoded({ extended: false }));
-    app.use(express.text({ type: '*/*' }));
     app.use(express.json());
 
     app.use((req, res, next) => {
